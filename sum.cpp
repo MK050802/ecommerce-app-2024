039f701da63bc171bad823c973274e4eee5e7f73
Megha <megha834004@gmail.com>
2024-05-16 18:13:39 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b)
+{
+    cout << "the sum of the entered two number is:" << endl;
+}
+int main()
+{
+    int ans = sum(3, 4);
+    cout << ans;
+    // return 0;
+}
